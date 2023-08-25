9c1503b073391992a6906a2aed9f770312f88ef1
paras <paraspalli1234@gmail.com>
2023-08-25 15:37:15 +0530

    Final V2.1 [Removed API]

--- lib/api.dart
+++ lib/api.dart
@@ -1,14 +1,11 @@
 class ApiConstants {
-  static String api = "5045929c-8d5d-4150-bfdf-2fefae5993c7";
-  static String api2 = "83c0bf0c-99f4-4d2a-8e17-71b98db1641b";
-  static String api3 = "d6a9a483-3b5e-4232-b7a9-0c63c47c4bba";
-  static String api4 = "9fc6fef2-44f0-4e7b-968e-17727c6b0549";
+  static String api = "Your API Key";
 
   static String currentMatches =
-      "https://api.cricapi.com/v1/currentMatches?apikey=$api2&offset=0";
+      "https://api.cricapi.com/v1/currentMatches?apikey=$api&offset=0";
 
   static String cricScores =
-      "https://api.cricapi.com/v1/cricScore?apikey=$api2&offset=0";
+      "https://api.cricapi.com/v1/cricScore?apikey=$api&offset=0";
 
   static bool matchType = true;
 }
